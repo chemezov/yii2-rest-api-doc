ebfa70f1f84b75a83b52dea28dfc51c9e2f05e3b
Mikhail Chemezov <michlenanosoft@gmail.com>
2020-02-07 14:33:00 +0500

    extend adding url rules for documentation frontend. useful when you use \yii\rest\UrlRule by default in your rest app.

--- Module.php
+++ Module.php
@@ -4,14 +4,21 @@ namespace chemezov\yii2\rest_api_doc;
 
 class Module extends \yii\base\Module implements \yii\base\BootstrapInterface
 {
-
     public $controllerNamespace = 'chemezov\yii2\rest_api_doc\controllers';
 
     public function bootstrap($app)
     {
         $app->getUrlManager()->addRules([
-            $this->id => $this->id . '/default/index',
-            $this->id . '/<controller:[\w\-]+>/<action:[\w\-]+>' => $this->id . '/<controller>/<action>',
-            ], false);
+            [
+                'class' => '\yii\web\UrlRule',
+                'pattern' => $this->id,
+                'route' => $this->id . '/default/index',
+            ],
+            [
+                'class' => '\yii\web\UrlRule',
+                'pattern' => $this->id . '/<controller:[\w\-]+>/<action:[\w\-]+>',
+                'route' => $this->id . '/<controller>/<action>',
+            ],
+        ], false);
     }
 }
